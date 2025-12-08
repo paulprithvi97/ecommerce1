579f4f11d31aa8bc3d345022b78d7d9d1dbde98e
Prithvi Paul <69955241+paulprithvi97@users.noreply.github.com>
2025-12-08 18:07:24 +0600

    Add routing and layout components; implement Home, Shop, and Error pages. (Home, Header, Footer Shown on the browser)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
+import RootLayout from "./components/layouts/RootLayout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Shop from "./components/pages/Shop";
+import Error from "./components/pages/Error";
 function App() {
-
-
-  return (  <>
+  return (
+   <>
+  <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
   
   </>
-)
-
+  )
 }
 
 export default App;
